77db226ba3bfb7d339514b63505e32c00b447c06
Sakshi <sakshis.1407@gmail.com>
2021-01-20 02:38:21 +0530

    setup firebase filestore

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = express()
 const port = 3000
 const static = express.static("public")
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./url-shortener-604c1-firebase-adminsdk-nxkt6-ea0cf36ee7.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const urlsdb = admin.firestore().collection("urlsdb")
+
 app.use(static)
 app.use(bodyParser.json())
 
